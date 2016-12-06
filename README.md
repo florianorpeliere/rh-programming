6e8c903ee225894c5c2ae8b648d2b1b6be6624f6
Florian Orpelière <florian.orpeliere@gmail.com>
2016-12-06 23:47:13 +0100

    :gift: You are not prepared

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
 # rh-programming
 
-Repo to teach RH to code
+## HTML
+
+On commence avec le HTML :).
+Vous devez réaliser les choses suivantes :
+
+* Dans la balise `<header>`, ajouter une balise `<h1>`, comprenant le titre `Todos` ou autre chose :cat:
+* Dans la balise `<header>`, ajouter un `input` text, avec comme classe `new-todo`. (Vous pouvez ajouter un placeholder avec votre pair).
+* Ajouter une liste (à vous de trouver la bonne balise) avec comme classe `todo-list`, dans la balise portant la classe `main`.
+
+## CSS
+
+Bon ben c'est bien gentil tout ça, mais va falloir customiser un peu tout ça.
+Dans le ficher rh.css, vous devez réaliser :
+
+* Compléter la règle `.todoapp h1` afin d'avoir une taille de police de 100px, le texte aligné au milieu et la couleur de votre choix.
+* Ajouter la règle pour la classe `todo-list` suivi d'un `li` pour obtenir une taille de police de 24px, une bordure de 1px solid de la couleur que vous souhaitez. Vous pouvez aussi customiser le background ;).
+
+Si vous souhaitez voir, ajouter le code suivant dans votre liste(<ul>).
+
+```html
+<li>
+  <div class="view">
+    <input class="toggle" type="checkbox">
+    <label>Alors ça marche ?</label>
+    <button class="destroy"></button>
+  </div>
+</li>
+```
+## JS
+
+Alors là, les choses se compliquent ...
+Je vous laisse compléter les morceaux manquants, tout est expliqué ;).
+
+Bon courage :gift:
--- app.js
+++ app.js
@@ -13,8 +13,219 @@
     Bon courage :)
   */
 
+  /*
+    Ici, c'est vos donnés.
+    listTodos représente l'ensemble des todos à afficher.
+    C'est un tableau contenant des éléments représentant un todo
+    Un todo est représenté comme suis
+
+    c'est un objet qui a un label (qui est une chaîne de caractères) et une valeur completed
+    pour savoir s'il est complété ou non.
+
+    Exemple :
+
+    {
+      label: "Je suis un todo",
+      completed: false
+    }
+
+  */
   var listTodos = [];
-  var list = document.querySelector('.todo-list');
+
+  /*
+    Ici ça représente l'élement HTML contenant les todos à afficher.
+    A vous de jouer !
+
+    Récupérer l'élement portant la classe .todo-list
+    Pensez à utiliser document qui représente votre HTML
+    Pensez à utiliser la fonction querySelector qui représente la façon de demander un élément à votre document
+  */
+  var list = //Hey, codez moi !
+
+  /*
+    Cette fonction permet d'ajouter un todo à votre tableau de todos
+    Comme vous pouvez le voir, il y a déjà un peu de code
+    Voyons ensemble
+  */
+  function ajouterUnTodo() {
+    /*
+      Ici nous allons récupérer ce qu'il y a d'écrit dans l'input text
+      C'est la donnée de notre todo
+    */
+    var nouveauTodoData = document.querySelector('.new-todo').value;
+
+    /*
+      Ici c'est à vous de jouer, vous devez ajouter un nouveau todo dans le tableau de todos
+      Pensez à utiliser la méthode push
+      Et le tableau s'appele listTodos
+    */
+
+    //Hey, codez moi !
+
+    /*
+      On efface ce qu'il y a d'écrit dans l'input
+    */
+    nouveauTodoData.value = '';
+
+    /*
+      On demande à afficher nos todos à l'écran, on va donc changer le HTML
+    */
+    afficher();
+  }
+
+  /*
+    C'est une fonction qui permet de changer votre HTML pour afficher les éléments
+    de votre tableau listTodos
+  */
+  function afficher () {
+    /*
+      Ici j'efface toute la liste HTML.
+      Je prends l'élément list que nous avions récupérer.
+    */
+    list.innerHTML = '';
+    /*
+      Je vais itérer au travers de mon tableau
+      A vous de compléter cette boucle
+    */
+    for (/* Hey, codez moi */) {
+      /*
+        Ici c'est une fonction un peu compliquer.
+        Elle permet de créer un élement HTML
+        Vous pouvez demander à votre Pair de vous expliquer
+      */
+      var todo = creerUnTodo(i, listTodos[i]);
+
+      /*
+        Ici j'ajoute des intéractions ( des évènements ;) ) pour cocher un todo et
+        pour le supprimer. Nous verrons cela un peu plus tard. Ne vous inquiétez pas.
+        Ignorez les pour le moment.
+      */
+      todo.querySelector('.toggle').addEventListener('click', cocherUnTodo);
+      todo.querySelector('.destroy').addEventListener('click', supprimerUnTodo);
+
+      /*
+        Ajouter l'element dans la liste HTML (attention pas dans le tableau ;) )
+        Pensez à utiliser la fonction appendChild
+        Pensez à utiliser l'objet todo créer juste au dessus
+      */
+
+      //Hey, codez moi !
+    }
+  }
+
+
+  /*
+    A partir d'ici, vous devriez pouvoir faire fonctionner un peu votre application !!!
+    Félicitation !!!
+
+    Vous pouvez continuer pour effacer et cocher un todo.
+  */
+
+
+
+  function cocherUnTodo(event) {
+    /*
+      On s'embête beaucoup ici :D
+      Mais on récupére l'élement cliqué, puis on remonte jusqu'à la balise <li>
+      qui représente le todo afin de récupérer son identifiant
+      Cette identifiant permet de faire la relation entre ce que vous voyez
+      et l'élement dans votre tableau.
+    */
+    var checkbox = event.target;
+    var todo = checkbox.parentElement.parentElement;
+    var id = todo.dataset.id;
+
+    /*
+      Ici vous devez cocher ou décocher votre todo.
+      Vous devez donc "inverser" la valeur de completed
+      Demander à votre Pair de vous expliquer la notion de boolean
+      Et inverser là :)
+    */
+
+    //Hey, codez moi !
+
+    afficher();
+  }
+
+
+  function supprimerUnTodo() {
+    /*
+      On s'embête beaucoup ici :D
+      Mais on récupére l'élement cliqué, puis on remonte jusqu'à la balise <li>
+      qui représente le todo afin de récupérer son identifiant
+      Cette identifiant permet de faire la relation entre ce que vous voyez
+      et l'élement dans votre tableau.
+    */
+    var checkbox = event.target;
+    var todo = checkbox.parentElement.parentElement;
+    var id = todo.dataset.id;
+
+    /*
+      Ici vous devez retirer un élément de votre tableau.
+      Vous avez sa "place" dans le tableau grâce à id
+      A vous de jouer :)
+      Pensez à utiliser splice (demander à votre Pair)
+      Pensez à utiliser listTodos
+    */
+
+    //Hey, codez moi !
+
+    afficher();
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   var nouveauTodoData = document.querySelector('.new-todo');
 
   nouveauTodoData.addEventListener('keyup', function(event) {
@@ -22,83 +233,6 @@
       ajouterUnTodo();
     }
   });
-
-  function ajouterUnTodo() {
-    var nouveauTodoData = document.querySelector('.new-todo');
-
-    listTodos.push(
-      {
-        label: nouveauTodoData.value,
-        completed: false
-      }
-    );
-
-    nouveauTodoData.value = '';
-
-    afficher();
-  }
-
-  function cocherUnTodo(event) {
-    var checkbox = event.target;
-    var todo = checkbox.parentElement.parentElement;
-    var id = todo.dataset.id;
-
-    listTodos[id].completed = !listTodos[id].completed;
-
-    afficher();
-  }
-
-  function supprimerUnTodo() {
-    var checkbox = event.target;
-    var todo = checkbox.parentElement.parentElement;
-    var id = todo.dataset.id;
-
-    listTodos.splice(id, 1);
-
-    afficher();
-  }
-
-  function afficher () {
-    list.innerHTML = '';
-    for (var i = 0; i < listTodos.length; ++i) {
-      var todo = creerUnTodo(i, listTodos[i]);
-      todo.querySelector('.toggle').addEventListener('click', cocherUnTodo);
-      todo.querySelector('.destroy').addEventListener('click', supprimerUnTodo);
-      list.appendChild(todo);
-    }
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
   function creerUnTodo(id, todo) {
     var todoLi = document.createElement('li');
